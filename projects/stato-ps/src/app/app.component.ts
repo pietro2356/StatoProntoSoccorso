c69818b0d7c9c32071f3ac7d088c0da05ff5a6b3
u17507 <pietro.rocchio@gpi.it>
2024-04-22 17:45:12 +0200

    Installato e configurato PrimeNG

--- projects/stato-ps/src/app/app.component.ts
+++ projects/stato-ps/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'sps-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
