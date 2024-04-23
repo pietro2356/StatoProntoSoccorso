c36cb6703f14f591ecdf669831a667f86aa9410a
u17507 <pietro.rocchio@gpi.it>
2024-04-23 23:32:09 +0200

    Creato modulo feature stato-ps e fixato logo in main-layout

--- projects/stato-ps/src/app/app.routes.ts
+++ projects/stato-ps/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'stato-ps',
+    pathMatch: 'full',
+  },
+  {
+    path: 'stato-ps',
+    loadChildren: () => import('./feature/stato-ps/stato-ps.module').then(m => m.StatoPsModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'stato-ps',
+    pathMatch: 'full',
+  },
+];
--- projects/stato-ps/src/app/layout/main-layout/main-layout.component.ts
+++ projects/stato-ps/src/app/layout/main-layout/main-layout.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MessagesModule } from 'primeng/messages';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { TitleCasePipe } from '@angular/common';
+import { NgOptimizedImage, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'sps-main-layout',
   standalone: true,
-  imports: [MessagesModule, RouterOutlet, RouterLink, RouterLinkActive, TitleCasePipe],
+  imports: [MessagesModule, RouterOutlet, RouterLink, RouterLinkActive, TitleCasePipe, NgOptimizedImage],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
